6f5c1d5c2f59f3b6d00a4519c4d5e36961683312
Sarah Smith <s.smith.nz@gmail.com>
2021-03-04 13:26:49 +1300

    add unicornClicked function

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -3,3 +3,40 @@
 // -    -   -   -   -  //
 
 console.log("Inflate The Unicorn!")
+//Using the onclick method to call it, you will need to write a function 
+//that gets called every time you click on one of the unicorns.
+
+var inflate = [0, 0, 0]
+
+
+function unicornClicked() {
+    var unicorn = event.target
+    var id = unicorn.id[7]
+    inflate[id]++
+      
+      if (inflate[id] === 4)
+      {inflate[id] = 0}
+      
+
+      if (inflate[id] === 3) {
+      alert ("Unicorn Number " + [id] + " says thank you!")}
+      
+
+      unicorn.src = "./images/unicorn-" + inflate[id] +".png"
+    }
+    
+  
+
+
+
+
+//if (unicorn.id === 'unicorn1') {
+  //inflate1++
+ /*if (document.getElementById('unicorn1').src = "./images/unicorn-0.png") {
+  document.getElementById('unicorn1').src = "./images/unicorn-1.png";
+} else if (document.getElementById("unicorn1").src = "./images/unicorn-1.png") {
+    (document.getElementById("unicorn1").src = "./images/unicorn-2.png") 
+} else if (document.getElementById("unicorn1").src = "./images/unicorn-2.png") {
+  document.getElementById("unicorn1").src = "./images/unicorn-3.png";
+}
+};*/
