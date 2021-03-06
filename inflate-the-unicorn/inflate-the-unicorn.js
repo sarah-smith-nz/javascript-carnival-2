e2219db9e72a7ccc75d19bb19086a9d983337572
Sarah Smith <s.smith.nz@gmail.com>
2021-03-06 15:14:26 +1300

    add sound and text

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -7,28 +7,28 @@ console.log("Inflate The Unicorn!")
 //that gets called every time you click on one of the unicorns.
 
 var inflate = [0, 0, 0]
-
+var audioElement1 = new Audio('horse.wav');
+var audioElement2 = new Audio('guitar.wav');
+var imageType = ['png','gif'];
 
 function unicornClicked() {
     var unicorn = event.target
     var id = unicorn.id[7]
     inflate[id]++
       
-      if (inflate[id] === 4)
-      {inflate[id] = 0}
-      
-
       if (inflate[id] === 3) {
-      alert ("Unicorn Number " + [id] + " says thank you!")}
+        inflate[id] = 0 
+        audioElement1.play()
+        alert ("Unicorn Number " + [id] + " says thank you!")
+      }
       
-
       unicorn.src = "./images/unicorn-" + inflate[id] +".png"
+      
     }
-    
   
-
-
-
+    function reset () {  
+      location = location  
+    };
 
 //if (unicorn.id === 'unicorn1') {
   //inflate1++
